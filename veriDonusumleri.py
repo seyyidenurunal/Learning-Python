2474cceba4d502187299ef87dfa9eaafefeccaa7
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-18 19:01:48 +0300

    Veri Dönüşümleri

---
+++ veriDonusumleri.py
@@ -0,0 +1,17 @@
+c = "12354" #Şeklinde bir string tanımladık
+
+print(int(c)) #bunu tamsayı değerine dönüştürmek için int fonksiyonunu kullanmamız gerekir.
+
+
+p = 12545 # Şeklinde bir tamsayı tanımlarsak
+
+print(str(p)) # Bu ifadeyi de str fonksiyonuyla stringe çevirebiliriz.
+
+z = 12.55 #Bir float tanımlayalım
+
+print(int(z)) # Bunu tamsayı olarak yazmak istediğimizde de int fonksiyonunu kullanırız.
+
+#Tam tersini yapmak istersek de 
+d = 12
+
+print(float(d)) #float fonksiyonunu kullanıyoruz. 
