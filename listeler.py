e7bde3cc0cb129a753a1bdcc2012ae3d60d03940
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-19 01:00:07 +0300

    Update listeler.py

--- listeler.py
+++ listeler.py
@@ -21,6 +21,9 @@ print(yeni)
 yeni_1 = pazar[2:]  # İkici elemandan(muz) sonraki bütün elemanları alır.
 print(yeni_1)
 yeni_2 = pazar[:3]  # Üçüncü elemandan(çilek) önceki bütün elemanları alır.
+print(yeni_2)
+yeni_3 = pazar [1:8:2] #1. elemandan 8. elemana kadar 2 şer 2 şer atlayarak gider. 
+print(yeni_3)
 
 print("6____________")
 esya = [ "sandalye", "masa", "koltuk", "halı", "perde"]
@@ -38,6 +41,8 @@ print(pazar.index("üzüm")) #Kaçıncı index numarasında olduğunu gösterir.
                            #Aynı değere sahip elemanlar için, önce gelen elemanın index numarasını verir. Örneğin; elma : 1 
 print("8____________") 
 pazar.pop() #Son elemanı listeden atar. 4 Numarada listeye kiraz elemanını eklemiştik, onun listede çıkması gerekir.
+pazar.pop(0) #index numarası verilen elemamı listeden atar.
+print(pazar)
 
 print("10____________") 
 pazar.remove("üzüm") #Seçilen elemanı listeden atar.  
@@ -50,3 +55,14 @@ print(pazar)
 print("8____________") 
 pazar.sort() #Alfabetik olarak sıralar.
 print(pazar)
+pazar.sort(reverse= True) #Alfabetik olarak tersten sıralar.
+print(pazar)
+
+print("9____________") 
+liste = list("Merhaba") #Her bir harfi ayrı bir liste elemanı olarak sıralamayı sağlar.
+print(liste)
+
+print("10____________") 
+liste_2 = [[1,2], [3,5],[7,9]] #İç içe listeler oluşturabiliriz.
+print(liste_2)
+print(liste_2[1][1]) #1 index numaralı listenin içindeki 1 index numaralı eleman.
