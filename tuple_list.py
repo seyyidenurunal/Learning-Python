95084c97d1fa0785a74d2bcd64d9e14dbcd81a90
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-19 14:40:08 +0300

    Update tuple_list.py

--- tuple_list.py
+++ tuple_list.py
@@ -1,19 +1,26 @@
-tuple_list = ( "ayşe", "fatma", "hasan", True , False , 123, 5454, 5645646, 4654, 54)
+tuple_list = ( "ayşe", "fatma", "hasan", True , False , 123, 5454, 5645646, 4654, 54) #Listeler değiştirilebilir ancak tuple'lar değiştirilemez.
 list_list = [ "ayşe", "fatma", "hasan", True , False , 123, 5454, 5645646, 4654, 54]
 
 print("1_________________")
-
-
 print(tuple_list) 
-
+print(type(tuple_list))
 print(dir(tuple_list))
+
+
 print("_"*100)
 print(dir(list_list))
 
 
 print("2_________________")
 
-print(len(tuple_list))
+print(len(tuple_list)) #Tuple'ın uzunluğunu verir
+
+print("3_________________")
+print(tuple_list.count("fatma")) #Belirtilen elemanın tuple içerisinde kaç tane olduğunu gösterir
+
+print("4_________________")
+print(tuple_list.index(True)) #Belirtilen elemanın index numarasını verir
+
 
 
 
