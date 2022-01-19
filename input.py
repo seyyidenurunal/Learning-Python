7ee9b1e5188fe17261f93097af06bf09f1616160
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-19 15:16:39 +0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+a = int(input("Lütfen yaşınızı giriniz")) #Alınan değerin sayı olarak algılanmasını sağlar.
+print(a * 3)
+
+
+x = int(input("Birinci sayı :"))
+y = int(input("İkinci sayı :"))
+z = int(input("Üçüncü sayı :"))
+
+print("Toplam:", x+y+z)
