a311e6a167baec33089ab4b9aad22ae5d59a7b42
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-26 16:25:12 +0300

    Update while-else.py

--- while-else.py
+++ while-else.py
@@ -1,3 +1,5 @@
+print("1____________") 
+
 sayi = 0
 
 while sayi < 5 :
@@ -8,3 +10,25 @@ else :
 
 
 print("İşlem Tamamlandı.")
+
+
+
+print("2____________") 
+
+i = 0
+
+while (i <= 10):
+    print ("i'nin değeri : ", i)
+    i += 1
+
+
+print("3____________") 
+
+liste= [1,2,3,4,5]
+
+index = 0
+
+while (index < len(liste)):
+    print("index: ", index, "Liste elemanı: ", liste[index])
+    index += 1 
+    
