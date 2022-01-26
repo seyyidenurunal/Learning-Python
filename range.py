c87bec3c7cc26728ef8b436411f7fc7e2c146df2
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-01-26 16:47:00 +0300

    Range Fonksiyonu

---
+++ range.py
@@ -0,0 +1,28 @@
+
+print("1____________") 
+
+print(*range(0,20))
+
+
+print("2____________") 
+
+print(*range(15))
+
+print("3____________") 
+
+print(*range(0,100,5))
+
+print("4____________")
+
+print(*range(50,0,-5))
+
+print("5____________")
+
+for t in range(1,101):
+    print(t)
+
+print("6____________")
+
+for i in range (1,20):
+    print(" * " * i) 
+
